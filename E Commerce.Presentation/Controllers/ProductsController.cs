5e965a5158c2ba8253a4c2f124e9daa9d11a219f
abdullahgh8900 <atg789000@gmail.com>
2025-11-15 10:59:29 +0200

    Product Module [Product Controller] & Add Registration in Program

---
+++ E Commerce.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using E_Commerce.Services_Abstraction;
+using E_Commerce.Shared.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Presentation.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProducts()
+    {
+        var products = await _productService.GetAllTypesAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+    {
+        var product = await _productService.GetProductByIdAsync(id);
+        return Ok(product);
+    }
+    
+    [HttpGet("brands")]
+    public async Task<ActionResult<IEnumerable<BrandDTO>>> GetAllBrands()
+    {
+        var brands = await _productService.GetAllBrandsAsync();
+        return Ok(brands);
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IEnumerable<TypeDTO>>> GetAllTypes()
+    {
+        var types = await _productService.GetAllTypesAsync();
+        return Ok(types);
+    }
+
+}
--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,12 +1,19 @@
 
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Persistence.Data.DataSeed;
 using E_Commerce.Persistence.Data.DbContexts;
+using E_Commerce.Persistence.Repositories;
+using E_Commerce.Services;
+using E_Commerce.Services.MappingProfiles;
+using E_Commerce.Services_Abstraction;
+using E_Commerce.Web.Extenions;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +23,27 @@ namespace E_Commerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<StoreDbContext>(options => {
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+            builder.Services.AddScoped<IDataInitializer, DataInitializer>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddAutoMapper(x => x.AddProfile<ProductProfile>());
+            builder.Services.AddScoped<IProductService, ProductService>();
 
             #endregion
 
+
             var app = builder.Build();
+
+
+            #region DataSeeding
+
+            await app.MigrateDatabaseAsync();
+            await app.SeedDatabaseAsync();
+
+            #endregion
 
             #region Configure the HTTP request pipeline
 
