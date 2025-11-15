96192655a0e18c685a989a83fd4de18cf599c54f
abdullahgh8900 <atg789000@gmail.com>
2025-11-15 10:47:14 +0200

    Genreic Repository - UnitOfWork  Async Model

---
+++ E Commerce.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Domain.Contracts;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync();
+
+    IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+}
