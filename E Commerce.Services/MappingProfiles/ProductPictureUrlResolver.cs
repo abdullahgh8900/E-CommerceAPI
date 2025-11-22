232fe937b96ec11017de9f50e087be36779d9964
abdullahgh8900 <atg789000@gmail.com>
2025-11-22 18:37:40 +0200

    Auto Mapper Picture Url Resolver

---
+++ E Commerce.Services/MappingProfiles/ProductPictureUrlResolver.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using E_Commerce.Domain.Entities.ProductModule;
+using E_Commerce.Shared.DTOs.ProductDTOs;
+using Microsoft.Extensions.Configuration;
+
+namespace E_Commerce.Services.MappingProfiles;
+
+public class ProductPictureUrlResolver : IValueResolver<Product, ProductDTO, string>
+{
+    private readonly IConfiguration _configuration;
+
+    public ProductPictureUrlResolver(IConfiguration  configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
+    {
+
+        if (string.IsNullOrEmpty(source.PictureUrl))
+            return string.Empty;
+
+        if (source.PictureUrl.StartsWith("http"))
+            return source.PictureUrl;
+        
+        var baseUrl = _configuration.GetSection("URLs")["BaseUrl"];
+
+        if (string.IsNullOrEmpty(baseUrl))
+            return string.Empty;
+
+        var pictureUrl = $"{baseUrl}{source.PictureUrl}";
+
+        return pictureUrl;
+
+    }
+}
--- E Commerce.Services/MappingProfiles/ProductProfile.cs
+++ E Commerce.Services/MappingProfiles/ProductProfile.cs
@@ -11,7 +11,8 @@ public class ProductProfile : Profile
     {
         CreateMap<Product, ProductDTO>()
             .ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(src => src.ProductBrand.Name))
-            .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src=> src.ProductType.Name));
+            .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src=> src.ProductType.Name))
+            .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<ProductPictureUrlResolver>());
         CreateMap<ProductBrand, BrandDTO>();
         CreateMap<ProductType, TypeDTO>();
     }
